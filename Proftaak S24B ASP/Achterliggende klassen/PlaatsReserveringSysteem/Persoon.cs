bc9408e2ca5a3ed5b9f174b8891fca996ae116be
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2015-06-18 14:04:10 +0200

    Klassen van klassendiagram in het project gestopt
    Er kunnen nog een aantal fouten in de klassen zitten, het klassendiagram
    moeten we later ook nog updaten aangezien er op dit moment daar ook nog
    flink wat fouten inzitten.

---
+++ Proftaak S24B ASP/Achterliggende klassen/PlaatsReserveringSysteem/Persoon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proftaak_S24B_ASP
+{
+    public class Persoon
+    {
+    }
+}
---
+++ Proftaak S24B ASP/Achterliggende klassen/PlaatsReserveringSysteem/Plek.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proftaak_S24B_ASP.Achterliggende_klassen.PlaatsReserveringSysteem
+{
+    public class Plek
+    {
+    }
+}
